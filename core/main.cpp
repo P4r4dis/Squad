27d73c0a7bfa701cc37c5e9e0605c47da49354a4
P4r4dis <adil.d.pro@gmail.com>
2023-01-12 02:46:25 +0100

    [UPDATE] - main.cpp update

--- core/main.cpp
+++ core/main.cpp
@@ -1,4 +1,15 @@
+#include "../part0_medic/include/Skat.hpp"
+
+#include <iostream>
+
 int     main(void)
 {
+    std::cout << "Skat Main Starting---------------------------:" << std::endl;
+    Skat    s("Junior", 5);
+    std::cout << "Soldier " << s.name() << std::endl;
+    s.status();
+    s.useStimPaks();
+    std::cout << "Skat Main Ending------------------------------" << std::endl;
+
     return 0;
 }
