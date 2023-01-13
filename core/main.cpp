b42cbddca1711acac9f36cb70459bea9e51e2856
P4r4dis <adil.d.pro@gmail.com>
2023-01-13 14:11:16 +0100

    [ADD] - KoalaBot's main function add to global main

--- core/main.cpp
+++ core/main.cpp
@@ -1,15 +1,22 @@
 #include "../part0_medic/include/Skat.hpp"
-
+#include "../part1_koalaBot/include/KoalaBot.hpp"
 #include <iostream>
 
 int     main(void)
 {
     std::cout << "Skat Main Starting---------------------------:" << std::endl;
-    Skat    s("Junior", 5);
+    Skat        s("Junior", 5);
     std::cout << "Soldier " << s.name() << std::endl;
     s.status();
     s.useStimPaks();
     std::cout << "Skat Main Ending------------------------------" << std::endl;
+    std::cout << std::endl;
 
+    std::cout << "KoalaBot Main Starting---------------------------:" << std::endl;
+    KoalaBot    kb;
+    std::cout << std::boolalpha << kb.status() << std::endl;
+    kb.informations();
+    std::cout << "KoalaBot Main Ending------------------------------" << std::endl;
+    std::cout << std::endl;
     return 0;
 }
