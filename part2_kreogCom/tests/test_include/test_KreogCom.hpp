91b5d0fe90d200c12d722c5224a9a0880d65c2ee
P4r4dis <adil.d.pro@gmail.com>
2023-01-13 17:34:49 +0100

    [ADD] - unit test for KreogCom::addCom add

--- part2_kreogCom/tests/test_include/test_KreogCom.hpp
+++ part2_kreogCom/tests/test_include/test_KreogCom.hpp
@@ -2,23 +2,31 @@
 #   define  __TEST_KREOGCOM__
 
 #include <string>
-
+#include <memory>
 
 void    redirect_all_stdout(void);
 
 class   KreogCom
 {
     private:
-        int         _x;
-        int         _y;
-        const int   m_serial;
+        int                         _x;
+        int                         _y;
+        const int                   m_serial;
+        KreogCom                    *_next;
+        // std::unique_ptr<KreogCom>   m_next;
     public:
         KreogCom(int x, int y, int serial);
         ~KreogCom(void);
 
-        int         getSerial(void) const;
-        int         getX(void);
-        int         getY(void);
+        int                         getSerial(void) const;
+        int                         getX(void);
+        int                         getY(void);
+
+        void                        addCom(int x, int y, int serial);
+        void                        addComSmartPtr(int x, int y, int serial);
+
+        KreogCom                    *getCom(void);
+        // std::unique_ptr<KreogCom>   getUniquePtrCom(void);
 };
 
 #endif //   !__TEST_KREOGCOM__
--- part2_kreogCom/tests/test_src/test_KreogCom.cpp
+++ part2_kreogCom/tests/test_src/test_KreogCom.cpp
@@ -12,17 +12,56 @@ KreogCom::~KreogCom(void)
     std::cout << "KreogCom " << m_serial << " shutting down" << std::endl;
 }
 
-int     KreogCom::getSerial(void) const
+int         KreogCom::getSerial(void) const
 {
     return m_serial;
 }
 
-int     KreogCom::getX(void)
+int         KreogCom::getX(void)
 {
     return _x;
 }
 
-int     KreogCom::getY(void)
+int         KreogCom::getY(void)
 {
     return _y;
 }
+
+void        KreogCom::addCom(int x, int y, int serial)
+{
+    // Create a new KreogCom with the given x, y, and serial values
+    KreogCom    *newKreogCom = new KreogCom(x, y, serial);
+
+    // Get the current next KreogCom
+    KreogCom    *oldKreogCom = getCom();
+
+    // If there is already a next KreogCom
+    if (oldKreogCom != nullptr) {
+        // Link the new KreogCom to the old one
+        newKreogCom->_next = oldKreogCom;
+    }
+    // Link the new KreogCom to the current one
+    _next = newKreogCom;
+}
+
+// void        KreogCom::addComSmartPtr(int x, int y, int serial)
+// {
+//     // Create a new KreogCom with the given x, y, and serial values
+//     std::unique_ptr<KreogCom> newKreogCom = std::make_unique<KreogCom>(x, y, serial);
+
+//     // Get the current next KreogCom
+//     std::unique_ptr<KreogCom> oldKreogCom = std::move(m_next);
+
+//     // Link the new KreogCom to the current one
+//     m_next = std::move(newKreogCom);
+// }
+
+KreogCom    *KreogCom::getCom(void)
+{
+    return _next;
+}
+
+// std::unique_ptr<KreogCom>   KreogCom::getUniquePtrCom(void)
+// {
+//     return std::move(m_next);
+// }
