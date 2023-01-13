61e21e531b861eb02923ad4936c8142733b4d504
P4r4dis <adil.d.pro@gmail.com>
2023-01-13 15:24:00 +0100

    [ADD] - unit test for KreogCom destruction add

--- part2_kreogCom/tests/test_KreogCom.cpp
+++ part2_kreogCom/tests/test_KreogCom.cpp
@@ -17,3 +17,15 @@ Test(KreogCom, test_KreogCom_ctor, .init = redirect_all_stdout)
     cr_assert(k.getSerial() == 101010);
     cr_assert_stdout_eq_str("KreogCom 101010 initialized\n");
 }
+
+Test(KreogCom, test_KreogCom_dtor, .init = redirect_all_stdout)
+{  
+    KreogCom    k(42, 42, 101010);
+
+    cr_assert(k.getX() == 42);
+    cr_assert(k.getY() == 42);
+    cr_assert(k.getSerial() == 101010);
+    k.~KreogCom();
+    cr_assert_stdout_eq_str("KreogCom 101010 initialized\n\
+KreogCom 101010 shutting down\n");
+}
