b2606485c439d4890eda9c1fe90ca166816b3d2a
P4r4dis <adil.d.pro@gmail.com>
2023-01-17 18:55:03 +0100

    [ADD] - feature Squad_Skat_constructor add

--- part4_g-squad/core_g-squad/main_g-squad.cpp
+++ part4_g-squad/core_g-squad/main_g-squad.cpp
@@ -1,12 +1,25 @@
 #include "../include/Skat.hpp"
 #include "../include/KoalaBot.hpp"
 #include "../include/Parts.hpp"
-#include "../include/KreogCom.hpp"
-#include "../include/Phaser.hpp"
+// #include "../include/KreogCom.hpp"
+// #include "../include/Phaser.hpp"
 
 #include <iostream>
 
 int     main(void)
 {
+    Skat    skat;
+    std::cout   << "skat.getName = " << skat.name() << std::endl;
+    std::cout   << "skat.getStimPaks = " << skat.stimPaks() << std::endl;
+    std::cout   << "skat.getKreogCom.getSerial = " 
+                << skat.getKreogCom().getSerial() << std::endl;
+    std::cout   << "skat.getKreogCom.getX = " 
+                << skat.getKreogCom().getX() << std::endl;
+    std::cout   << "skat.getKreogCom.getY = " 
+                << skat.getKreogCom().getY() << std::endl;
+    std::cout   << "skat.getPhaser.getMaxAmmo = " 
+                << skat.getPhaser().getMaxAmmos() << std::endl;
+    std::cout   << "skat.getPhaser.getAmmoType = " 
+                << skat.getPhaser().getAmmoType() << std::endl;
     return 0;
 }
--- part4_g-squad/include/Skat.hpp
+++ part4_g-squad/include/Skat.hpp
@@ -2,14 +2,24 @@
 #   define  __SKAT__
 
 #include <string>
+#include "KreogCom.hpp"
+#include "Phaser.hpp"
 class   Skat
 {
     private:
         std::string             _name;
         int                     _stimPaks;
+        KreogCom                _kreogCom;
+        Phaser                  _phaser;
     public:
-        Skat(const std::string& name = "bob", int stimPaks = 15);
+        Skat(const std::string& name = "bob",
+            int stimPaks = 15, int serial = 0,
+            int x = 0, int y = 0,
+            Phaser::AmmoType type = Phaser::REGULAR);
         ~Skat();
+
+        const std::string&      getName(void);
+        int                     getStimPaks(void);
 
         int&                    stimPaks(void);
         const std::string       &name(void);
@@ -18,6 +28,8 @@ class   Skat
         void                    useStimPaks(void);
         void                    status(void) const;
 
+        KreogCom                getKreogCom(void);
+        Phaser                  getPhaser(void);
 };
 
 
