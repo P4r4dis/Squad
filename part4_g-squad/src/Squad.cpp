aae258f47a5e127f7cd211fceec532258c3dcbc0
P4r4dis <adil.d.pro@gmail.com>
2023-01-24 15:24:16 +0100

    [UPDATE] - architecture update for project

--- part4_g-squad/src/Squad.cpp
+++
@@ -1,1 +0,0 @@
-#include "../include/Squad.hpp"
--- part4_g-squad/tests/test_src/test_Src.cpp
+++
@@ -1,8 +0,0 @@
-#include "../test_include/test_Phaser.hpp"
-#include <criterion/redirect.h>
-
-void    redirect_all_stdout(void)
-{
-    cr_redirect_stdout();
-    cr_redirect_stderr();
-}
